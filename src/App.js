d2e7ac84cbe91b32777270248a516b5baff036e9
am26y2 <amitraj9046@gmail.com>
2020-11-12 13:18:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { evaluate } from "mathjs";
 function App() {
+  const [expression, setExpression] = useState(0);
+  const handleclick = (value) => {
+    let newExpression = expression + value.toString();
+    setExpression(newExpression);
+  };
+  const handleResult = () => {
+    let result;
+    try {
+      result = evaluate(expression);
+      setExpression(result);
+    } catch (err) {
+      setExpression("Invalid Exprssion!");
+    }
+  };
+  const deletekey = () => {
+    if (expression.length >= 0 && expression !== 0) {
+      let newExpression = expression.substr(0, expression.length - 1);
+      console.log(newExpression.length);
+      if (newExpression.length <= 0) setExpression(0);
+      else setExpression(newExpression);
+    } else {
+      console.log(expression.length);
+    }
+  };
+  const clearDisplay = () => {
+    setExpression(0);
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +46,79 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="container">
+        <div>
+          <h1>Simple Calculator</h1>
+        </div>
+        <div className="display">{expression}</div>
+        <div className="buttons">
+          <div className="row">
+            <button id="clear" onClick={clearDisplay}>
+              Ac
+            </button>
+            <button id="delete" onClick={deletekey}>
+              DELETE
+            </button>
+            <button id="equal" onClick={handleResult}>
+              =
+            </button>
+            <button id="divide" onClick={() => handleclick("/")}>
+              /
+            </button>
+          </div>
+          <div className="row">
+            <button id="7" onClick={(id) => handleclick(7)}>
+              7
+            </button>
+            <button id="8" onClick={() => handleclick(8)}>
+              8
+            </button>
+            <button id="9" onClick={() => handleclick(9)}>
+              9
+            </button>
+            <button id="*" onClick={() => handleclick("*")}>
+              *
+            </button>
+          </div>
+          <div className="row">
+            <button id="4" onClick={(id) => handleclick(4)}>
+              4
+            </button>
+            <button id="5" onClick={() => handleclick(5)}>
+              5
+            </button>
+            <button id="6" onClick={() => handleclick(6)}>
+              6
+            </button>
+            <button id="subtract" onClick={() => handleclick("-")}>
+              -
+            </button>
+          </div>
+          <div className="row">
+            <button id="1" onClick={(id) => handleclick(1)}>
+              1
+            </button>
+            <button id="2" onClick={() => handleclick(2)}>
+              2
+            </button>
+            <button id="3" onClick={() => handleclick(3)}>
+              3
+            </button>
+            <button id="add" onClick={() => handleclick("+")}>
+              -
+            </button>
+          </div>
+          <div className="row">
+            <button id="0" onClick={() => handleclick(0)}>
+              0
+            </button>
+            <button id="percentile" onClick={() => handleclick("%")}>
+              %
+            </button>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
